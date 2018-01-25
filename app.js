7ef40e1eca6797e05f8e38fa7a41271ad46c034c
Margus Andrei <Margus.A@live.com>
2018-01-25 20:40:51 +0200

    2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path =require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/nodema');
 let db = mongoose.connection;
@@ -25,6 +26,12 @@ let Meme = require('./models/meme');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Body Oarser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 //Home Route
 app.get('/', function(req, res){
   Meme.find({}, function(err, memes){
@@ -44,6 +51,23 @@ app.get('/memes/add', function(req, res){
   res.render('add_meme',{
     title: 'Add Meme'
   });
+});
+
+//Add Submit POST Route
+app.post('/memes/add', function(req, res){
+let meme = new Meme();
+meme.title = req.body.title;
+meme.author = req.body.author;
+meme.body = req.body.body;
+
+meme.save(function(err){
+  if(err){
+    console.log(err);
+    return;
+  } else {
+    res.redirect('/');
+  }
+ });
 });
 
 //Start Server
